f5faa38d0df5feda89fd93d92fc16d112b823783
Michael Eden <70781525+Deltt@users.noreply.github.com>
2025-08-30 01:41:08 +0200

    Added webrtc

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const http = require("http");
 const WebSocket = require("ws");
+const { RTCPeerConnection } = require("wrtc");
+
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer();
 const wss = new WebSocket.Server({ server });
 
+const clients = new Map();
+
 // --- Signaling logic ---
 wss.on("connection", ws => {
+	const pc = new RTCPeerConnection();
+	let dc;
+
+  // Create a data channel for the client
+  dc = pc.createDataChannel("game");
+  dc.onmessage = (msg) => {
+    // Broadcast received data to all other clients
+    for (const [otherWs, other] of clients.entries()) {
+      if (otherWs !== ws && other.dataChannel?.readyState === "open") {
+        other.dataChannel.send(msg.data);
+      }
+    }
+  };
+
+  clients.set(ws, { pc, dataChannel: dc });
+
   ws.on("message", message => {
     // Relay all messages to other clients
     wss.clients.forEach(client => {
